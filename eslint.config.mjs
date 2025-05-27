83434ee051c62acc3f1230754995ce72bc9bbc03
ThEWoLf882-42 <agimi@student.1337.ma>
2025-05-27 22:22:49 +0100

    eslint no-console

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,9 +26,10 @@ export default tseslint.config(
   },
   {
     rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );

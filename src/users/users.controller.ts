53f8e81be1a5a08faf04cab5d09c4ec696541bb5
ThEWoLf882-42 <agimi@student.1337.ma>
2025-05-27 15:16:07 +0100

    Users Controller & Service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  create(@Body() user: CreateUserDto) {
+    return this.usersService.create(user);
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.usersService.findId(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(user: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    return this.prisma.user.create({
+      data: {
+        email: user.email,
+        password: hashedPassword,
+      },
+    });
+  }
+
+  async findEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findId(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+}

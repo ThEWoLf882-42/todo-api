8c30ab05c145795c4cb8b7d11bff153ddd3f8972
ThEWoLf882-42 <agimi@student.1337.ma>
2025-05-28 16:01:43 +0100

    Todos Complete filter fixes

--- src/todos/dto/filter-todo.dto.ts
+++ src/todos/dto/filter-todo.dto.ts
@@ -1,7 +1,13 @@
+import { Transform } from 'class-transformer';
 import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class FilterTodoDto {
   @IsOptional()
+  @Transform(({ value }) => {
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    return value;
+  })
   @IsBoolean()
   completed?: boolean;
 
